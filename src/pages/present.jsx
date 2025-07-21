ee9ee5708662004622b4246a21f9e67e1db527a7
supaKd <kevin.khek@gmail.com>
2025-07-21 17:14:17 +0200

    Ajout effet sable animé sur la section équipe

--- src/pages/present.jsx
+++ src/pages/present.jsx
@@ -1,8 +1,15 @@
+// Presentation.jsx
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 import { motion } from "framer-motion";
+import "swiper/css";
+import "swiper/css/autoplay";
 
-function Present() {
+const images = ["ado.webp", "ado.webp", "ado.webp"]; // Ajoute ici tes images
+
+function Presentation() {
   return (
-    <section className="presentation">
+    <section className="presentation" id="#present">
       <motion.div
         className="text"
         initial={{ opacity: 0, x: -50 }}
@@ -15,30 +22,36 @@ function Present() {
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum, eius
           ducimus expedita, nulla dolorem aperiam vitae, itaque nihil quis ad
           labore dolorum modi doloribus? Ullam necessitatibus blanditiis iure
-          minima rerum. Blanditiis dicta, necessitatibus, cumque neque
-          voluptatum in consequuntur voluptates dignissimos facilis dolorem quod
-          nulla iusto qui porro obcaecati error officiis distinctio consequatur
-          molestiae officia nam corporis eius? Sunt, consectetur aliquam! Ipsa,
-          qui sint architecto ut pariatur dolorum iure sapiente. Adipisci alias
-          provident cumque id sequi deleniti voluptatum voluptatibus amet, fugit
-          eos! Temporibus illo, itaque fugit vero dignissimos unde obcaecati
-          deleniti. quibusdam dolorem neque a maxime deleniti, eligendi beatae
-          dolore. Architecto odit, doloribus expedita maxime eum veniam earum
-          magni modi vero culpa. Ea, autem in.
+          minima rerumducimus expedita, nulla dolorem aperiam vitae, itaque nihil quis ad
+          labore dolorum modi doloribus? Ullam necessitatibus blanditiis iure
+          minima rerum...
         </p>
       </motion.div>
 
       <motion.div
-        className="img"
+        className="slider"
         initial={{ opacity: 0, x: 50 }}
         whileInView={{ opacity: 1, x: 0 }}
         transition={{ duration: 0.6, ease: "easeOut", delay: 0.2 }}
         viewport={{ once: true }}
       >
-        <img src="ado.webp" alt="img-presentation" />
+        <Swiper
+          modules={[Autoplay]}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          speed={800}
+          loop={true}
+          spaceBetween={20}
+          slidesPerView={1}
+        >
+          {images.map((src, index) => (
+            <SwiperSlide key={index}>
+              <img src={src} alt={`slide-${index}`} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </motion.div>
     </section>
   );
 }
 
-export default Present;
+export default Presentation;
